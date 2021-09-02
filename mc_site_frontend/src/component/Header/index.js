91afb50b4e1d5931e60e8e24b9e64699a5ffb95a
YuanGao-MAU <gaoyuan_hz@qq.com>
2021-09-01 18:04:37 -0700

    added the nav bar

--- mc_site_frontend/src/component/Header/index.js
+++ mc_site_frontend/src/component/Header/index.js
@@ -3,17 +3,28 @@ import React,{Component} from "react";
 export default class Header extends Component {
     render() {
         return(
-            <ul class="nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style={{color:"white"}}>Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style={{color:"white"}}>Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" style={{color:"white"}}>Link</a>
-                </li>
-            </ul>
+            <div>
+  
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style={{color:"white",fontSize:"30px"}}>HelloWorldMC</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style={{color:"white",fontSize:"30px"}}>加入</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style={{color:"white",fontSize:"30px"}}>服规</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style={{color:"white",fontSize:"30px"}}>帮助</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style={{color:"white",fontSize:"30px"}}>近期活动</a>
+                    </li>
+                </ul>
+
+            </div>
+
         )
     }
 }
